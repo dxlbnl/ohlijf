500dc65c96e3d9d5b8d8b4c8b81147b432b90c70
Dexter <dexter@dxlb.nl>
2024-01-16 13:09:19 +0100

    speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,10 @@
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	const menu = menuSchema.parse(menuData);
 </script>
