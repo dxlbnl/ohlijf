8d98a9946a539c39271d83184501aed86c51f387
Dexter <dexter@dxlb.nl>
2024-01-16 12:57:50 +0100

    Adding analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 	import '../app.css';
 
 	import Navigation from '$lib/Navigation.svelte';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navigation />
