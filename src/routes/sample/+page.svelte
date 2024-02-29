758243690227a5f32fa2cbf6911860f56f948f10
Dexter <dexter@dxlb.nl>
2024-02-29 14:54:33 +0100

    Testing

--- src/routes/sample/+page.svelte
+++ src/routes/sample/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import Image from '$lib/components/Image.svelte';
+	import image from './hoofdpijn.jpg';
 </script>
 
 <Image
 	image={{
-		sourceUrl: '/images/home/hoofdpijn.jpg'
+		sourceUrl: image
 	}}
 />
