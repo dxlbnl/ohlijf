4b5099f18c103a67fbf04835b3a17fb64d99618b
Dexter <dexter@dxlb.nl>
2024-09-11 13:52:54 +0200

    right request

--- src/routes/(tools)/webhook/+server.ts
+++ src/routes/(tools)/webhook/+server.ts
@@ -1,6 +1,11 @@
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request }) => {
+	if (request.headers.get('Content-Type') !== 'application/json') {
+		console.log('No JSON');
+		return new Response('Expected JSON', { status: 400 });
+	}
+
 	const data = await request.json();
 	console.log('Received data', data);
 	return new Response();
