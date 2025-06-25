094cddd5aa3f9461b54b3ad182af30cb56b10b7f
RobinOhLijf <160025241+RobinOhLijf@users.noreply.github.com>
2025-06-25 15:01:08 +0200

    Update homepage enters.svelte

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -15,9 +15,9 @@
 	<p class="subheader white">Voor vrouwen met chronische hoofdpijn en ADHD(-kenmerken)</p>
 	<p class="subheader white">
 		Huh? Kan ik van mijn hoofdpijn afkomen? Hoef ik hier niet mee te ‘leren leven’?
-Nee, zeker niet! Wij leefden meer dan 10 jaar met zo’n beetje non-stop hoofdpijn- tot we ontdekten dat het ook anders kan.
-Met een totaal andere aanpak leer je wat jouw klachten écht proberen te zeggen. We combineren wetenschappelijke inzichten over het zenuwstelsel en de mindbody-connectie met humor, herkenning en een diepe kijk in jouw unieke ADHD-brein.
-Je hebt al van alles geprobeerd en doet zó hard je best. Maar herstel begint met het ánders doen — en dat leren we jou!
+Nee, zeker niet! Wij leefden meer dan 10 jaar met zo’n beetje non-stop hoofdpijn- tot we ontdekten dat het ook anders kan.</p>
+<p>Met een totaal andere aanpak leer je wat jouw klachten écht proberen te zeggen. We combineren wetenschappelijke inzichten over het zenuwstelsel en de mindbody-connectie met humor, herkenning en een diepe kijk in jouw unieke ADHD-brein.</p>
+<p>Je hebt al van alles geprobeerd en doet zó hard je best. Maar herstel begint met het ánders doen — en dat leren we jou!
 	</p>
 	<p class="subheader white bold">
 		Nieuwsgierig of jouw hoofdpijn te maken heeft met hoe jij bedraad bent? Doe de test
@@ -44,7 +44,8 @@ Je hebt al van alles geprobeerd en doet zó hard je best. Maar herstel begint me
 				<p>Wij – Aurinke & Robin – weten hoe het is.</p>
 				<p>De hoofdpijn die elke dag overschaduwt. De gedachten die maar doorrazen, zonder pauzeknop. De overprikkeling, het gevoel dat je alles ‘goed’ moet doen, en niet goed kunnen ontspannen. </p>
 				<p>Wij zijn ervaringsdeskundigen én vrouwen met ADHD. We hebben zelf jarenlang met hoofdpijn en migraine geworsteld — en weten hoe uitzichtloos dat kan voelen.
-Juist ons drukke brein, ons gevoelige zenuwstelsel en die neiging tot ‘altijd aanstaan’ maakten ons extra vatbaar voor pijn. Maar... diezelfde eigenschappen bleken ook de sleutel tot herstel.
+Juist ons drukke brein, ons gevoelige zenuwstelsel en die neiging tot ‘altijd aanstaan’ maakten ons extra vatbaar voor pijn. </p>
+				<p>Maar... diezelfde eigenschappen bleken ook de sleutel tot herstel.
 </p>
 				<a href="/methode" target="_blank">Hoe dan?</a>
 			</article>
