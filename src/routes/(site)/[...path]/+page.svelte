4770b97f03b57490b19a51217d86a4e6a5fb6b84
Dexter <dexter@dxlb.nl>
2026-04-12 10:51:07 +0200

    404 pagina

---
+++ src/routes/(site)/[...path]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import PageHeader from '$lib/components/PageHeader.svelte';
+</script>
+
+<PageHeader titel="Pagina niet gevonden" />
+
+<main class="content">
+	<img src="404.svg" alt="404" />
+	<h1>Oh nee! We kunnen de pagina niet vinden..</h1>
+
+	<a href="/" class="button">Terug naar homepage</a>
+</main>
+
+<style>
+	main {
+		justify-items: center;
+	}
+</style>
